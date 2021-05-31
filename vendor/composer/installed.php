8a258467329b4429a6423ab2bb9863ebef3a750f
buddy <buddy@buddy.works>
2021-05-31 23:16:44 +0000

    5

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '0.1.0',
+    'version' => '0.1.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'buddy/buddy-deploy',
+  ),
+  'versions' => 
+  array (
+    'buddy/buddy-deploy' => 
+    array (
+      'pretty_version' => '0.1.0',
+      'version' => '0.1.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
