f59c0a6d31ee84760a5f0425f0ed469343599962
Agnieszka Palmowska <a.palmowska@freshpixels.pl>
2021-05-31 15:15:09 +0200

    new plugin design

---
+++ templates/header.php
@@ -0,0 +1,15 @@
+<?php
+namespace BuddyIntegration;
+
+?>
+<header class="buddy-header buddy-grid grid-columns-2">
+    <div class="item">
+        <a class="buddy-logo" href="" title=""><img src="<?php echo Config::get( 'plugin_url' ); ?>assets/images/deploy-buddy-logo.svg" alt="Deploy Buddy - WordPress Plugin"></a>
+    </div>
+    <nav class="item">
+        <ul>
+            <li><a href="https://buddy.works/pricing" target="_blank">Try Buddy</a></li>
+            <li><a href="">Report an issue</a></li>
+        </ul>
+    </nav>
+</header>
